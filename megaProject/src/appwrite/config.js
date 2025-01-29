5139bf3bdb7a8c6119fc6ff3a5c2fe70bcd8f545
vedantsalvi10 <vedantsalvi10@gmail.com>
2025-01-29 16:49:24 +0530

    appwrite authentication

---
+++ megaProject/src/appwrite/config.js
@@ -0,0 +1,120 @@
+import {conf} from "../conf/conf.js";
+import { Client, Databases, ID, Query, Storage } from "appwrite";
+
+export class Service {
+ client = new Client()
+ database;
+ storage;
+ constructor (){
+  this.client.setEndpoint(conf.appwrite_url)
+  .setProject(conf.appwrite_Project_ID);
+  database = new Databases(this.client);
+  storage = new Storage(this.client);
+ }
+ async addData({tittle,slug,content,image,status,userID}){
+   try{
+     return await this.database.createDocument(
+      conf.appwrite_Database,
+      appwrite_Collection,
+      slug,
+      {
+        tittle,
+        content,
+        image,
+        status,
+        userID
+      }
+     )
+   }catch(error){
+    console.log("APPWRITE PROJECT :: CREATEDATA ERROR :: ",error);
+   }
+ }
+ async updateData(slug,{tittle,content,image,status}){
+  try{
+    return await this.database.updateDocument(
+      conf.appwrite_Database,
+      conf.appwrite_Collection,
+      slug,
+      {
+        tittle,
+        content,
+        image,
+        status
+      }
+    )
+  }catch(error){
+   console.log("APPWRITEERROR :: UPDATEDATABASE ERROR :: ",error);
+  }
+ }
+ async deleteData(slug){
+  try{
+   await this.database.deleteDocument(
+      conf.appwrite_Database,
+      conf.appwrite_Collection,
+      slug
+   )
+   return true;
+  }catch(error){
+    console.log("APPWRITEPROJECT :: DELETEDOCUMENTERROR :: ",error);
+    return false;
+  }
+ }
+async getPost(slug){
+  try{
+    return await this.database.getDocument(
+      conf.appwrite_Database,
+      conf.appwrite_Collection,
+      slug
+    )
+    
+  }catch(error){
+    console.log("APPWRITEERROR :: GETPOST ERROR :: ",error);
+    return false;
+  }
+}
+async viewPost(query = [Query.equal('status','active')]){
+try{
+ return await this.database.listDocuments(
+      conf.appwrite_Database,
+      conf.appwrite_Collection,
+      query,
+      100,
+      0
+ )
+}catch(error){
+  console.log("APPWRITEERROR :: GETPOST ERROR :: ",error);
+    return false;
+}
+}
+async uploadFile(file){
+  try{
+   return await this.storage.createFile(
+    conf.appwrite_Bucket,
+    ID.unique(),
+    file
+   )
+  }catch(error){
+    console.log("APPWRITEPROJECT :: UPLOADFILE ERROR :: ",error);
+    return false
+  }
+}
+async deleteFile(fileID){
+  try{
+    await this.storage.deleteFile(
+        conf.appwrite_Bucket,
+        fileID
+    );
+    return true;
+  }catch(error){
+    console.log("APPWRITEPROJECT :: DELETEFILE ERROR :: ",error);
+    return false;
+  }
+}
+filePreview(fileID){
+  conf.appwrite_Bucket,
+  fileID
+}
+}
+
+const service = new Service()
+export default service;
