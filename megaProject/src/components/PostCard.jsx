e5bc559c341d0397560feea012a8f5874a2ffc8b
vedantsalvi10 <vedantsalvi10@gmail.com>
2025-01-31 21:49:47 +0530

    making components for authentication

---
+++ megaProject/src/components/PostCard.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import appwriteService from "../appwrite/config.js"
+import {Link} from "react-router-dom"
+export function PostCard($id,tittle,image){
+  return(
+    <Link to={`/post/${$id}`}>
+    <div className='w-full bg-gray-100 rounded-xl p-4'>
+      <div className='w-full justify-center mb-4'>
+        <image href={appwriteService.filePreview(image) }  alt={tittle} className='rounded-xl'/>
+      </div>
+      <h2 className='text-xl font-bold'>{tittle}</h2>
+    </div>
+    </Link>
+  )
+}
--- megaProject/src/components/initial.js
+++ megaProject/src/components/initial.js
@@ -3,4 +3,6 @@ import { Footer } from "./footer/Footer.jsx";
 import { Logout } from "./header/Logout.jsx";
 import { Container } from "./Container.jsx";
 import { Logo } from "./Logo.jsx";
-export  {Header,Footer,Logo,Container,Logout};
+import { Button } from "./button.jsx";
+
+export  {Header,Footer,Logo,Container,Logout,Button};
